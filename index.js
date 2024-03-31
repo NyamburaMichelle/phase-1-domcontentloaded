be313c5e8a26843117e207e8f51f7449fbb27756
Michelle <nyamburamichelle400@gmail.com>
2024-03-31 23:02:51 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code goes here
+// // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    // console.log("The DOM has loaded");
+    addEvent()
+  });
+  
+  function addEvent() {
+    let text = document.getElementById("text")
+    text.innerHTML = "This is really cool!"
+  }
+
+
